6bfd25baa0775d0b2afef80c29e6446028233af7
Thomas Mayrhofer <thomas@mayrhofer.at>
2021-01-12 23:37:58 +0100

    content: add images from 2021

--- resize_images.sh
+++ resize_images.sh
@@ -1,5 +1,9 @@
 # /bin/bash
 
+(cd images/gallery/fulls && rename --force 'y/A-Z/a-z/' *)
+
 /usr/local/Cellar/graphicsmagick/1.3.35/bin/gm mogrify -resize 1290x1290 images/gallery/fulls/*.jpg;
+
+rm -rf images/gallery/thumbs/*
 
 (cd images/gallery/fulls && /usr/local/Cellar/graphicsmagick/1.3.35/bin/gm mogrify -create-directories -output-directory ../thumbs -resize '360x360^' -gravity center -extent 360x360 ./*.jpg)
