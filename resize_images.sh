2fbf0f42aea6e7335830e2d1c4b82fedb48cf0a0
Thomas Mayrhofer <thomas@mayrhofer.at>
2021-04-18 11:48:12 +0200

    feat: add images

--- resize_images.sh
+++ resize_images.sh
@@ -2,8 +2,8 @@
 
 (cd images/gallery/fulls && rename --force 'y/A-Z/a-z/' *)
 
-/usr/local/Cellar/graphicsmagick/1.3.35/bin/gm mogrify -resize 1290x1290 images/gallery/fulls/*.jpg;
+/usr/local/Cellar/graphicsmagick/1.3.36/bin/gm mogrify -resize 1290x1290 images/gallery/fulls/*.jpg;
 
 rm -rf images/gallery/thumbs/*
 
-(cd images/gallery/fulls && /usr/local/Cellar/graphicsmagick/1.3.35/bin/gm mogrify -create-directories -output-directory ../thumbs -resize '360x360^' -gravity center -extent 360x360 ./*.jpg)
+(cd images/gallery/fulls && /usr/local/Cellar/graphicsmagick/1.3.36/bin/gm mogrify -create-directories -output-directory ../thumbs -resize '360x360^' -gravity center -extent 360x360 ./*.jpg)
